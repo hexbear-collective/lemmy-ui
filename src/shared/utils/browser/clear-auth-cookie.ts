7d1210b336cdbbd4dc5c0deeabc359690951b23d
Anon <makotech222@gmail.com>
2023-09-08 19:29:25 -0500

    fix logging out

--- src/shared/utils/browser/clear-auth-cookie.ts
+++ src/shared/utils/browser/clear-auth-cookie.ts
@@ -1,10 +1,12 @@
 import * as cookie from "cookie";
 import { authCookieName } from "../../config";
+import { getExternalHost } from "@utils/env";
 
 export default function clearAuthCookie() {
   document.cookie = cookie.serialize(authCookieName, "", {
     maxAge: -1,
     sameSite: true,
     path: "/",
+    domain: `.${getExternalHost()}`
   });
 }
