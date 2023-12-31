9954618b4eb1607b672b20e929049c2ce9c6ba9d
SleeplessOne1917 <insomnia-void@protonmail.com>
2023-12-31 15:53:45 -0500

    Add auto-update time

--- src/shared/components/home/moscow-time.tsx
+++ src/shared/components/home/moscow-time.tsx
@@ -4,14 +4,41 @@ import { Component } from "inferno";
 const intlFormat = Intl.DateTimeFormat(undefined, {
   timeZone: "Europe/Moscow",
   timeStyle: "full",
+  dateStyle: "short",
 });
 
-export default class MoscowTime extends Component {
+interface MoscowTimeState {
+  currentDate: Date;
+}
+
+export default class MoscowTime extends Component<any, MoscowTimeState> {
+  private dateInterval?: NodeJS.Timeout;
+
+  constructor(props: any, context: any) {
+    super(props, context);
+
+    this.state = {
+      currentDate: new Date(),
+    };
+  }
+
+  componentDidMount() {
+    this.dateInterval = setInterval(() => {
+      this.setState({
+        currentDate: new Date(),
+      });
+    }, 1000);
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.dateInterval);
+  }
+
   render() {
     return (
       isBrowser() && (
         <div>
-          Current time:&nbsp;
+          <strong>Current time:</strong>&nbsp;
           {intlFormat.format(new Date())}
         </div>
       )
