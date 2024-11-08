bf0ae3d12de0a9e1b06f7a78738b1ce4cf240262
Anon <makotech222@gmail.com>
2024-11-08 10:33:07 -0600

    Add Custom Hexbear code

--- src/shared/utils/app/initialize-site.ts
+++ src/shared/utils/app/initialize-site.ts
@@ -2,7 +2,13 @@ import { GetSiteResponse } from "lemmy-js-client";
 import { setupMarkdown } from "../../markdown";
 import { UserService } from "../../services";
 
-export default function initializeSite(site?: GetSiteResponse) {
+export default function initializeSite(
+  site?: GetSiteResponse,
+  is_server = false
+) {
   UserService.Instance.myUserInfo = site?.my_user;
-  setupMarkdown();
+  if (site) {
+    //setupEmojiDataModel(site.custom_emojis ?? []);
+  }
+  setupMarkdown(is_server);
 }
